5ae9d22b259095984cc8cf15559ad725174068d9
Jhony Vidal <joliveira@sms-latam.com>
2020-06-26 23:22:22 -0300

    Add Login Page Layout

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -7,8 +7,104 @@ class LoginPage extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           _createBackground(context),
+          _loginForm(context),
         ],
       )
+    );
+  }
+
+  Widget _loginForm(context) {
+
+    final size = MediaQuery.of(context).size;
+
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+
+          SafeArea(
+            child: Container (
+              height: 180.0,
+            ),
+          ),
+
+          Container(
+            width: size.width * 0.85,
+            padding: EdgeInsets.symmetric(vertical: 50.0),
+            margin: EdgeInsets.symmetric(vertical: 50.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5.0),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 3.0,
+                  offset: Offset(0.0, 5.0),
+                  spreadRadius: 3.0
+                )
+              ]
+            ),
+            child: Column(
+              children: <Widget>[
+                Text('LogIn', style: TextStyle(fontSize: 20.0)),
+                SizedBox( height: 60.0 ),
+                _createEmail(),
+                SizedBox( height: 30.0 ),
+                _createPassword(),
+                SizedBox( height: 10.0 ),
+                _createLoginButton()
+              ],
+            ),
+          ),
+
+          Text('Forgot Password?'),
+          SizedBox( height: 100.0 )
+        ]
+      )
+    );
+  }
+
+  Widget _createPassword() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          icon: Icon(Icons.alternate_email,color: Colors.deepPurple),
+          labelText: 'Password',
+        ),
+      ),
+    );
+  }
+
+  Widget _createEmail() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon(Icons.lock,color: Colors.deepPurple),
+          hintText: 'ejemplo@email.com',
+          labelText: 'Email',
+        ),
+      ),
+    );
+  }
+
+  Widget _createLoginButton() {
+    return RaisedButton(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+        child: Text('LogIn'),
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5.0)
+      ),
+      elevation: 0.0,
+      color: Colors.deepPurple,
+      textColor: Colors.white,
+      onPressed: (){},
     );
   }
 
